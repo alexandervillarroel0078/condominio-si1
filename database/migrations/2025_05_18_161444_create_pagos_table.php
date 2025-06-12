d467a932be42d1c332578f9410d05f253e8508a8
Nelson Chumacero <nelsonchumacero755@gmail.com>
2025-06-12 02:22:53 -0400

    pago multa

--- database/migrations/2025_05_18_161444_create_pagos_table.php
+++ database/migrations/2025_05_18_161444_create_pagos_table.php
@@ -8,7 +8,8 @@ return new class extends Migration {
     public function up(): void {
         Schema::create('pagos', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('cuota_id')->constrained()->onDelete('cascade');
+            $table->foreignId('cuota_id')->nullable()->constrained('cuotas')->onDelete('cascade');
+            $table->foreignId('multa_id')->nullable()->constrained('multas')->onDelete('cascade');
             $table->decimal('monto_pagado', 10, 2);
             $table->date('fecha_pago');
             $table->string('metodo')->nullable(); // efectivo, QR, Stripe, etc.
